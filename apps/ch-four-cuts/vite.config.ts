779b97ae9e9a463ef3413a3b3527a45c75933df0
nabi.lee <hello@nabi.kim>
2023-11-08 23:02:59 +0900

    chore(ch-four-cuts): vite alias 설정을 root config 에도 추가

--- apps/ch-four-cuts/vite.config.ts
+++ apps/ch-four-cuts/vite.config.ts
@@ -1,9 +1,15 @@
 import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 import vike from 'vike/plugin';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
   plugins: [react(), vike()],
+  resolve: {
+    alias: {
+      '#': resolve('./src'),
+    },
+  },
 };
 
 export default config;
