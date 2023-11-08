ddc470c62af9c02171d9dac8bdf7debc07c5826e
nabi.lee <hello@nabi.kim>
2023-11-08 23:14:57 +0900

    chore(ch-four-cuts): satoriRouter를 제거

--- apps/ch-four-cuts/server/routes/index.ts
+++ apps/ch-four-cuts/server/routes/index.ts
@@ -1,12 +1,10 @@
 import { publicProcedure, router } from '../trpc.js';
 import { cameraRouter } from './camera.js';
 import { printerRouter } from './printer.js';
-import { satoriRouter } from './satori.js';
 import { sessionRouter } from './session.js';
 import { settingsRouter } from './settings.js';
 
 export const appRouter = router({
-  satori: satoriRouter,
   printer: printerRouter,
   camera: cameraRouter,
   session: sessionRouter,
