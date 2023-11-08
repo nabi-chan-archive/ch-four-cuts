0cce2969ea02a44a35949b5c283c4191727e6128
nabi.lee <hello@nabi.kim>
2023-11-09 03:13:05 +0900

    chore(viewer): initial commit

---
+++ apps/viewer/src/main.tsx
@@ -0,0 +1,16 @@
+import { BezierProvider } from '@ch-four-cuts/bezier-design-system';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = document.getElementById('root');
+if (!root) {
+  throw new Error('root element not found');
+}
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <BezierProvider>
+      <App />
+    </BezierProvider>
+  </React.StrictMode>,
+);
