98d5958dbfc60b1d77da8ecdbc25ee0fcc44bbed
nabi.lee <hello@nabi.kim>
2023-11-09 13:04:28 +0900

    chore(ch-four-cuts): postinstall hook에 prisma generate 코드를 추가

--- apps/ch-four-cuts/scripts/postinstall.js
+++ apps/ch-four-cuts/scripts/postinstall.js
@@ -1,6 +1,9 @@
 import { exec } from 'node:child_process';
 const arch = process.arch;
 
+console.log('create prisma client');
+exec('pnpm prisma generate', (_, stdout) => console.log(stdout));
+
 if (arch === 'arm64') {
   console.log('rebuild printer package for arm64');
   exec('cd node_modules/printer && node-gyp rebuild --arch=arm64', (_, stdout) => console.log(stdout));
