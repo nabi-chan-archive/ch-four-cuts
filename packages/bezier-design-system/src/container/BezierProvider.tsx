69b5c75375bc65add8c3c33fcb95ec8174746b91
nabi.lee <hello@nabi.kim>
2023-11-10 13:55:21 +0900

    feat(bezier-design-system): initial Foundation을 찾을 수 있도록 설정

--- packages/bezier-design-system/src/container/BezierProvider.tsx
+++ packages/bezier-design-system/src/container/BezierProvider.tsx
@@ -9,18 +9,20 @@ import { type ReactNode, useEffect, useState } from 'react';
 import GlobalStyles from '../styles/GlobalStyles';
 
 interface BezierProviderProps {
+  initialFoundation?: Foundation;
   children: ReactNode;
 }
 
-function BezierProvider({ children }: BezierProviderProps) {
-  const [foundation, setFoundation] = useState<Foundation>(LightFoundation);
+function BezierProvider({ initialFoundation, children }: BezierProviderProps) {
+  const [foundation, setFoundation] = useState<Foundation>(initialFoundation ?? LightFoundation);
 
   useEffect(() => {
+    if (initialFoundation) return;
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
       return setFoundation(DarkFoundation);
     }
     return setFoundation(LightFoundation);
-  }, []);
+  }, [initialFoundation]);
 
   return (
     <BaseBezierProvider foundation={foundation}>
