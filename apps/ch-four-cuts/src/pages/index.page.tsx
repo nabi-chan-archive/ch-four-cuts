d1c68ef598480356b1eb7bde941aea5915e5c040
nabi.lee <hello@nabi.kim>
2024-04-02 13:53:11 +0900

    fix(ch-four-cuts): 카메라 / 프린터가 연결되어있지 않은 경우에도 동작하도록 변경

--- apps/ch-four-cuts/src/pages/index.page.tsx
+++ apps/ch-four-cuts/src/pages/index.page.tsx
@@ -1,8 +1,22 @@
 import { Button, ButtonSize, Icon, IconSize, Text, Typography } from '@ch-four-cuts/bezier-design-system';
 import { ChannelBtnFilledIcon } from '@ch-four-cuts/bezier-design-system/icons';
+import { navigate } from 'vike/client/router';
+import { useSettingsQuery } from '#/features/Settings/queries/useSettingsQuery';
 import * as Styled from './index.styled';
 
 function Page() {
+  const { cameraConnected, cameraConnect, printerConnected, printerConnect } = useSettingsQuery();
+
+  async function handleClickButton() {
+    if (!cameraConnected) {
+      await cameraConnect.mutateAsync();
+    }
+    if (!printerConnected) {
+      await printerConnect.mutateAsync();
+    }
+    navigate('/app/intro');
+  }
+
   return (
     <Styled.Container>
       <Styled.Wrapper>
@@ -13,9 +27,7 @@ function Page() {
         <Text typo={Typography.Size18} marginBottom={24}>
           당신의 인생샷을 채널 네컷으로!
         </Text>
-        <a href="/app/intro">
-          <Button text="시작하기 ✨" size={ButtonSize.XL} />
-        </a>
+        <Button text="시작하기 ✨" size={ButtonSize.XL} onClick={handleClickButton} />
       </Styled.Wrapper>
     </Styled.Container>
   );
